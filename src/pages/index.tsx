a0b7333dc41cb0a7c1106e04692ba9176ce913e7
Xavier Perri <xdpxigaming@gmail.com>
2025-04-01 00:14:55 +1100

    Reformatted Code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,37 +1,43 @@
-import Head from 'next/head';
-import Script from 'next/script';
-import dynamic from 'next/dynamic';
+import Head from "next/head";
+import Script from "next/script";
+import dynamic from "next/dynamic";
 
-const Footer = dynamic(() => import('../components/Footer'), {ssr: false});
+const Footer = dynamic(() => import("../components/Footer"), { ssr: false });
 
 export default function Home() {
-    return (
-        <>
-            <Head>
-                <title>XDPXI</title>
-                <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-                <link href="/favicon.ico" rel="icon" type="image/ico"/>
-                <link rel="preconnect" href="https://kit.fontawesome.com"/>
-                <link rel="dns-prefetch" href="https://kit.fontawesome.com"/>
-            </Head>
+  return (
+    <>
+      <Head>
+        <title>XDPXI</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link href="/favicon.ico" rel="icon" type="image/ico" />
+        <link
+          rel="preload"
+          href="/favicon.ico"
+          as="image"
+          type="image/x-icon"
+        />
+        <link rel="preconnect" href="https://kit.fontawesome.com" />
+        <link rel="dns-prefetch" href="https://kit.fontawesome.com" />
+      </Head>
 
-            <Script
-                src="https://kit.fontawesome.com/7ea2a77467.js"
-                crossOrigin="anonymous"
-                strategy="lazyOnload"
-            />
+      <Script
+        src="https://kit.fontawesome.com/7ea2a77467.js"
+        crossOrigin="anonymous"
+        strategy="lazyOnload"
+      />
 
-            <header className="element" role="banner">
-                <h1>X D P X I</h1>
-            </header>
+      <header className="element" role="banner">
+        <h1>X D P X I</h1>
+      </header>
 
-            <main className="content-wrapper" role="main">
-                <section aria-label="Main Content" className="section">
-                    {/* Main content goes here */}
-                </section>
-            </main>
+      <main className="content-wrapper" role="main">
+        <section aria-label="Main Content" className="section">
+          {/* Main content goes here */}
+        </section>
+      </main>
 
-            <Footer/>
-        </>
-    );
+      <Footer />
+    </>
+  );
 }
