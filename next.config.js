9257abbdd3a53854ff04b8827b1b8042e2e741f0
Xavier Perri <xdpxigaming@gmail.com>
2025-03-31 23:33:44 +1100

    Reformatted Code

--- next.config.js
+++ next.config.js
@@ -1,23 +1,23 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  reactStrictMode: true,
-  poweredByHeader: false,
-  compress: true,
-  productionBrowserSourceMaps: false,
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "xdpxi.vercel.app",
-      },
-    ],
-    formats: ["image/avif", "image/webp"],
-  },
-  experimental: {
-    optimizeCss: true,
-    scrollRestoration: true,
-  },
+    reactStrictMode: true,
+    poweredByHeader: false,
+    compress: true,
+    productionBrowserSourceMaps: false,
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "xdpxi.vercel.app",
+            },
+        ],
+        formats: ["image/avif", "image/webp"],
+    },
+    experimental: {
+        optimizeCss: true,
+        scrollRestoration: true,
+    },
 };
 
 export default nextConfig;
