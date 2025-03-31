e76a156b34f86b3d33b3cd17678ff7b2f1c0c753
Xavier Perri <xdpxigaming@gmail.com>
2025-04-01 00:19:25 +1100

    Fixed White Flashing

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,25 @@
-import '../styles/globals.css';
-import type {AppProps} from 'next/app';
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
 
-function App({Component, pageProps}: AppProps) {
-    return <Component {...pageProps} />;
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="color-scheme" content="dark" />
+        <link rel="preload" href="../styles/globals.css" as="style" />
+        <style dangerouslySetInnerHTML={{
+          __html: `
+            body {
+              background-color: #121212;
+            }
+          `
+        }} />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default App;
