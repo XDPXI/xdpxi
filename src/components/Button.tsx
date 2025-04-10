71b1b49503592b567e86c2dba87d0f25d2ac083e
Xavier Perri <xdpxigaming@gmail.com>
2025-04-11 00:07:38 +1000

    Reformatted Code

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,18 +1,18 @@
 import React from "react";
 
 interface ButtonProps {
-    onClick: () => void;
-    children: React.ReactNode;
+  onClick: () => void;
+  children: React.ReactNode;
 }
 
-export default function Button({children, onClick}: ButtonProps) {
-    return (
-        <button
-            className="button"
-            onClick={onClick}
-            style={{margin: "0 10px 10px 0"}}
-        >
-            {children}
-        </button>
-    );
+export default function Button({ children, onClick }: ButtonProps) {
+  return (
+    <button
+      className="button"
+      onClick={onClick}
+      style={{ margin: "0 10px 10px 0" }}
+    >
+      {children}
+    </button>
+  );
 }
--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 
 interface TitleProps {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }
 
-export default function Title({children}: TitleProps) {
-    return (
-        <>
-            <header role="banner">
-                <h1>{children}</h1>
-            </header>
-        </>
-    );
+export default function Title({ children }: TitleProps) {
+  return (
+    <>
+      <header role="banner">
+        <h1>{children}</h1>
+      </header>
+    </>
+  );
 }
