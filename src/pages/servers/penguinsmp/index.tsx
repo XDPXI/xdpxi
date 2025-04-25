6ec6972b01da398a93e350b4f9edcc58c4d889cd
XDPXI <xdpxigaming@gmail.com>
2025-04-25 18:09:12 +1000

    Create index.tsx

---
+++ src/pages/servers/penguinsmp/index.tsx
@@ -0,0 +1,47 @@
+﻿import Header from "~/components/Header";
+import Footer from "~/components/Footer";
+import Content from "~/components/Content";
+import Button from "~/components/Button";
+
+export default function Home() {
+    return (
+        <>
+            <Header>Penguin SMP</Header>
+
+            <Content>
+                <div className="section" style={{textAlign: "center"}}>
+                    <div className="warning">
+                        <span className="warning-icon"><i
+                            aria-hidden="true"
+                            className="fa-solid fa-triangle-exclamation"
+                        ></i></span>
+                        <h2>
+                            This server is still in development!
+                        </h2>
+                    </div>
+                    <div className="warning">
+                        <span className="warning-icon"><i
+                            aria-hidden="true"
+                            className="fa-solid fa-list-check"
+                        ></i></span>
+                        <h2>
+                            Status: Setting up modpack...
+                        </h2>
+                    </div>
+                </div>
+
+                <div className="section" style={{textAlign: "center"}}>
+                    <Button
+                        onClick={() => {
+                            window.open("https://modrinth.com/modpack/penguin-smp", "_blank");
+                        }}
+                    >
+                        Modrinth
+                    </Button>
+                </div>
+            </Content>
+
+            <Footer/>
+        </>
+    );
+}
