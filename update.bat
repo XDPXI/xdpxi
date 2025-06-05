0474dafb5759c781912b197b567d1f7cb375e66c
XDPXI <xdpxigaming@gmail.com>
2025-06-05 13:22:56 +1000

    Delete update.bat

--- update.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-echo == Updating JavaScript dependencies (pnpm)... ==
-start /wait "" cmd /c "@echo off & pnpm update"
-start /wait "" cmd /c "@echo off & pnpm install"
-
-echo == All dependencies updated! ==
-pause
