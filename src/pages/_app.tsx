09d3b047169965f34df982b26b1d92d4f9c21bf1
XDPXI <xdpxigaming@gmail.com>
2025-09-04 18:38:27 +1000

    Fix statsig

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,51 +2,8 @@ import "../styles/globals.css";
 import "../styles/fontawesome.css";
 import type {AppProps} from "next/app";
 import Head from "next/head";
-import {StatsigProvider, useClientAsyncInit} from '@statsig/react-bindings';
-import {StatsigAutoCapturePlugin} from '@statsig/web-analytics';
-import {StatsigSessionReplayPlugin} from '@statsig/session-replay';
-import {useEffect, useMemo, useState} from 'react';
-
-const generateUserID = (): string => {
-    const array = new Uint8Array(16);
-    if (typeof window !== 'undefined') {
-        window.crypto.getRandomValues(array);
-    }
-    return 'user-' +
-        Array.from(array)
-            .map((byte) => ('0' + byte.toString(36)).slice(-2))
-            .join('')
-            .substring(0, 13);
-};
-
-const useSession = () => {
-    const [userID, setUserID] = useState<string>('');
-
-    useEffect(() => {
-        const storedUserID = localStorage.getItem('userID');
-        if (storedUserID) {
-            setUserID(storedUserID);
-        } else {
-            const newID = generateUserID();
-            localStorage.setItem('userID', newID);
-            setUserID(newID);
-        }
-    }, []);
-
-    return useMemo(() => ({userID}), [userID]);
-};
 
 export default function App({Component, pageProps}: AppProps) {
-    const {userID} = useSession();
-
-    const {client} = useClientAsyncInit(
-        'client-XzKBehpfICv80UVUZnVEtKwEYNgkIZU4FY3YkBYThXf',
-        {userID: userID},
-        {
-            plugins: [new StatsigAutoCapturePlugin(), new StatsigSessionReplayPlugin()]
-        },
-    );
-
     return (
         <>
             <Head>
@@ -62,9 +19,7 @@ export default function App({Component, pageProps}: AppProps) {
                     }}
                 />
             </Head>
-            <StatsigProvider client={client}>
-                <Component {...pageProps} />
-            </StatsigProvider>
+            <Component {...pageProps} />
         </>
     );
 }
