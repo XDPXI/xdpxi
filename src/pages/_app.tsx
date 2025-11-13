792673bcbc182a40650230a22fb542d9d9979991
Xavier Perri <contact@xdpxi.dev>
2025-11-13 12:45:23 +1100

    Fix databuddy

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@ import "../styles/globals.css";
 import "../styles/fontawesome.css";
 import type {AppProps} from "next/app";
 import Head from "next/head";
-import {Databuddy} from '@databuddy/sdk';
+import {Databuddy} from "@databuddy/sdk/react";
 
 export default function App({Component, pageProps}: AppProps) {
     return (
