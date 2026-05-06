c580460ea8d273aec02a73bb1ac8d44b3da5d679
Xavier Perri <contact@xdpxi.dev>
2026-05-06 11:25:43 +1000

    Add shopify redirect

---
+++ src/pages/shopify.tsx
@@ -0,0 +1,15 @@
+import Header from "~/components/Header";
+import Footer from "~/components/Footer";
+import Redirect from "~/components/Redirect";
+
+export default function Shopify() {
+  return (
+    <>
+      <Header>Shopify</Header>
+
+      <Redirect sitekey="" redirectSite="https://shopify.pxf.io/VO6Mq3" />
+
+      <Footer />
+    </>
+  );
+}
