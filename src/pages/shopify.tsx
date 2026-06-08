64de960a12135f77e6ed6d5ceff01d7b6fe5c2db
Xavier Perri <contact@xdpxi.dev>
2026-06-09 08:32:17 +1000

    Add event tracking for neyhost & shopify

--- src/pages/shopify.tsx
+++ src/pages/shopify.tsx
@@ -1,8 +1,14 @@
+import { useEffect } from "react";
+import { track } from "@databuddy/sdk/react";
 import Header from "~/components/Header";
 import Footer from "~/components/Footer";
 import Redirect from "~/components/Redirect";
 
 export default function Shopify() {
+  useEffect(() => {
+    track("visit_shopify");
+  }, []);
+
   return (
     <>
       <Header>Shopify</Header>
